36a23dc468df2da99e80e0fe5d882b01ce7ab439
Ryll974 <jullien.patrick@gmail.com>
2022-01-26 12:58:32 +0400

    séparation des widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,24 @@
 import 'package:flutter/material.dart';
+import 'inputPage.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text(
-            "Calculateur IMC",
-            style: TextStyle(color: Colors.purpleAccent,fontSize: 30.0, fontWeight: FontWeight.bold,),
-          ),
-          backgroundColor: Colors.black,
-        ),
-        body: InputPage(),
-      ),
-    ),
-  );
+  runApp(const IMCApp());
 }
 
-class InputPage extends StatefulWidget {
-  const InputPage({Key? key}) : super(key: key);
+class IMCApp extends StatelessWidget {
+  const IMCApp({Key? key}) : super(key: key);
 
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Center(),
+    return MaterialApp(
+      home: InputPage(),
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        appBarTheme: AppBarTheme(color: Color(0xFF0A0E21)),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        colorScheme:
+            ColorScheme.fromSwatch().copyWith(secondary: Colors.purple),
+      ),
     );
   }
 }
-
